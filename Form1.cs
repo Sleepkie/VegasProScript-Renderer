3b7be2ba762c0ecf28476dcf385de0dba02a37db
Daniil Ponomarev <slipkinpark1@gmail.com>
2023-04-29 16:58:47 +0300

    Добавил комменты

--- Form1.cs
+++ Form1.cs
@@ -26,24 +26,40 @@ public class EntryPoint
         //audioEvent.End = videoEndPoint;
 
         //Renderer renderer = vegas.Renderers.FindByName("6 Mbps HD 720-30p Video");
+
+        //выбор рендерера и шаблона рендера
         Renderer renderer = vegas.Renderers.FindByRendererID(13);
         RenderTemplate renderTemplate = renderer.Templates[0];
 
         
         for (int i = 0; i < 2; i++ )
         {
-
+            //приравниваем скорость видеофрагмента к переменной-множителю скорости
             videoEvent.PlaybackRate = playbackRate;
+            //во время каждой итерации увеличиваем множитель
             playbackRate += 0.5;
+
+
             //videoEndPoint = videoEvent.End;
+
+            //устанавливаем длину региона для рендера
             vegas.Transport.LoopRegionLength = videoEndPoint;
+
+            //аргументы для рендерера (пока не юзаю)
             RenderArgs renderArgs = new RenderArgs() { OutputFile = "C:\\Programmi\\DotNet\\ScriptVegas\\ScriptForms\\", RenderTemplate = renderTemplate };
+
+            //переменные для хранения формата файла
             string fileFormat = renderer.FileExtension;
             string fm = fileFormat.Trim('*');
+
+            //путь и название файла на выходе рендера (интерполяцию вегас не понимает)
             StringBuilder fileName =  new StringBuilder(String.Format("C:\\Programmi\\DotNet\\ScriptVegas\\ScriptForms\\rendered{0}{1}", (i + 1), fm));
             string fn = fileName.ToString();
+
+            //рендер
             vegas.Project.Render(fn, renderTemplate, new Timecode(0), videoEndPoint );
-            
+
+            //после рендера записываем новую длительность следующего рендера с учетом множителя скорости.
             Timecode changedLentgh  = new Timecode(videoEvent.End.ToMilliseconds()/playbackRate);
             videoEndPoint = changedLentgh;
         }
